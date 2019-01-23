5cff55cf3b0e02a1880630f856f95b53c32f8488
Jay Yoon <yoon10@cooper.edu>
2019-01-22 20:41:12 -0500

    message

---
+++ area.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.14
+
+void main()
+{
+	int radius = 2;
+	float area = PI * radius * radius;
+	printf("Area of circle with radius %d = %f\n",radius, area);
+	return;
+}
